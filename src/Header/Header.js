f50abe9ece3e8ee1c0180a38cf1d80db22ce2551
Soraya Benson <soraya.benson@gmail.com>
2021-02-09 15:05:39 -0800

    file tree created

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CreatureHeader extends React.Component {
+    render() {
+        return (
+            <header className="creatureHeader">
+                <h1 className="h1">creeeeeeeeatures</h1>
+            </header>
+        );
+    }
+}
